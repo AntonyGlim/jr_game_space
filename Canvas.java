f23e46362b8ee9bbced617ea53692b9f8a9c5ea9
Antony.Glim <Antony.Glim@gmail.com>
2019-06-26 22:23:35 +0300

    9.

--- Canvas.java
+++ Canvas.java
@@ -33,6 +33,24 @@ public class Canvas {
         }
     }
 
+    public void clear(){
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                matrix[i][j] = ' ';
+            }
+        }
+    }
+
+    public void print(){
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                System.out.print(matrix[i][j]);
+            }
+            System.out.println();
+        }
+        System.out.println("\n\n");
+    }
+
     public int getWidth() {
         return width;
     }
